e21a3614653733679210797cd8e40a70c3ef0d2e
Vishal-Chavan7 <96495893+Vishal-Chavan7@users.noreply.github.com>
2026-06-11 14:55:00 +0530

    product route updated

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import validateMiddleware from '../middlewares/validateMiddleware.js';
-import { createProductSchema } from '../validations/product.validation.js';
-import { createProduct } from '../controllers/product.controller.js';
+import { createProductSchema, updateProductSchema } from '../validations/product.validation.js';
+import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct     } from '../controllers/product.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import authorizeRole from '../middlewares/authorizeRole.middleware.js';
 
 const router = express.Router();
 
 router.route('/products')
-             .post(authMiddleware, authorizeRole('admin'), validateMiddleware(createProductSchema), createProduct);
+             .post(authMiddleware, authorizeRole('admin'), validateMiddleware(createProductSchema), createProduct)
+             .get(getAllProducts)
+
+router.route('/products/:id')
+             .get(getProductById)
+             .patch(authMiddleware, authorizeRole('admin'), validateMiddleware(updateProductSchema), updateProduct)
+             .delete(authMiddleware, authorizeRole('admin'), deleteProduct);
 
 export default router;
